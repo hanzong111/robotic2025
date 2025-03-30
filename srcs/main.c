c3dd3953011e789453610f9986315c79bceab7f1
Hanz Ong <hanzong111@gmail.com>
2025-03-30 21:06:07 +0800

    main.c

--- srcs/main.c
+++ srcs/main.c
@@ -1,16 +1,44 @@
 #include "../includes/robot.h"
-int main() {
-    Grid my_grid;
-    initialize_grid(&my_grid);
-    
-    printf("Initial 5×5 Grid (Row 0: Entry, Row 4: Drop-off):");
-    print_grid(&my_grid);
 
-    get_positions(&my_grid, 'R');  // Red ores
-    get_positions(&my_grid, 'B');  // Blue ores
-    
+void    print_checks(Grid map)
+{
     printf("\nFinal Grid:");
-    print_grid(&my_grid);
+    print_grid(&map);
+    Grid fresh;
+    copy_grid(&fresh.cells, &map.cells);
+    printf("\nCopied Grid:");
+    print_grid(&fresh);
+    print_ore_positions(&map);
+}
 
+int main() {
+    Grid            map;
+    Robot           robot;
+    path            p;
+    Position        goal = {0, 2};
+
+    grid_init(&map);
+    robot_init(&robot);
+    
+    //Ask for position of ores and store them in the ore_positions struct
+    get_positions(&map, 'R');  // Red ores
+    get_positions(&map, 'B');  // Blue ores
+    print_checks(map);
+    while (1)
+    {
+        printf("\nEnter new target cell (x y): ");
+        int h = scanf("%d %d", &goal.x, &goal.y);
+        (void)h;
+        a_star(&p, robot.pos, goal, &map);
+        int k = convert_path_to_instructions(p.path, p.path_length, &robot);
+        printf("Path (length %d):\n", p.path_length);
+            for (int i = 0; i < p.path_length; i++) {
+                printf("(%d, %d) ", p.path[i].x, p.path[i].y);
+            }
+        printf("\nMovement instructions:\n");
+        for (int i = 0; i < k; i++) {
+            printf("%s ->", robot.instructions[i]);
+        }
+    }
     return 0;
 }
