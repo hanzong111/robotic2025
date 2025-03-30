3df7794580aa9f803ea169358c100c714f76ce45
Hanz Ong <hanzong111@gmail.com>
2025-03-30 21:05:36 +0800

    Added new grid util functions

--- srcs/grid.c
+++ srcs/grid.c
@@ -1,6 +1,6 @@
 #include "../includes/robot.h"
 
-void initialize_grid(Grid *grid) {
+void grid_init(Grid *grid) {
     // Initialize all cells to '0'
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
@@ -12,19 +12,13 @@ void initialize_grid(Grid *grid) {
     for (int j = 0; j < COLS; j++) {
         grid->cells[4][j] = 'D';
     }
-}
 
-void print_grid(const Grid *grid) {
-    printf("\n");
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            char c = grid->cells[i][j];
-            if (c == 'R') printf(RED);      // Red
-            else if (c == 'B') printf(BLUE);  // Blue
-            else if (c == 'D') printf(GREEN);  // Green drop-off
-            else printf("\033[0;97m%c ", c);            // White
-        }
-        printf("\033[0m\n");
+    // Set all ore positions to -1
+    for (int k = 0; k < 4; k++) {
+        grid->blue[k].x = -1;
+        grid->blue[k].y = -1;
+        grid->red[k].x = -1;
+        grid->red[k].y = -1;
     }
 }
 
@@ -54,8 +48,32 @@ void get_positions(Grid *grid, char ore_type) {
             else 
             {
                 grid->cells[row][col] = ore_type;
+                if(ore_type == 'R') {
+                    grid->red[i].x = col;
+                    grid->red[i].y = row;
+                } else {
+                    grid->blue[i].x = col;
+                    grid->blue[i].y = row;
+                }
                 break;
             }
         }
     }
+}
+
+void copy_grid(char (*dest)[5][5], char (*src)[5][5]) {
+    memcpy(dest, src, 5 * 5 * sizeof(char));
+}
+
+// Function to update a cell in the grid
+// Returns true if update was successful, false if coordinates were invalid
+bool update_map(Grid *grid, int x, int y, char new_char) {
+    // Check if coordinates are within bounds
+    if (x < 0 || x >= COLS || y < 0 || y >= ROWS) {
+        return false;  // Invalid coordinates
+    }
+    
+    // Update the cell
+    grid->cells[y][x] = new_char;  // Note: y comes first for rows, then x for columns
+    return true;
 }
